9521455176dd273830d032279ae79513c1663746
taramouse <nickykeyse@gmail.com>
2019-05-07 18:10:51 +0100

    Formatted.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,9 @@
 <template>
   <section class="container xs-border xs-text-5 md-text-4">
-
-    <BaelHeader :blogtitle="blogtitle" :thecrumb="this.$store.state.theCrumb" :posts="blogposts" />
+    <BaelHeader :blogtitle="blogtitle" :thecrumb="this.$store.state.theCrumb" :posts="blogposts"/>
     <nuxt/>
     <SlideOut/>
-    <BaelFooter :pagination="paginate" />
+    <BaelFooter :pagination="paginate"/>
   </section>
 </template>
 <script>
@@ -48,7 +47,7 @@ export default {
   },
   computed: {
     paginate() {
-return this.$store.state.pagination
+      return this.$store.state.pagination;
     },
     blogposts() {
       return this.$store.state.blogPosts;
@@ -80,39 +79,43 @@ body {
   font-weight: 400;
   line-height: 1;
 }
-.feat-wrapper {max-height:55vh;width:100%;}
+.feat-wrapper {
+  max-height: 55vh;
+  width: 100%;
+}
 .slide-left-enter,
 .slide-right-leave-active {
   transform: translate(50%, 0);
-  opacity:0;
-  transition: all .25s;
+  opacity: 0;
+  transition: all 0.25s;
 }
 .slide-left-leave-active,
 .slide-right-enter {
   transform: translate(-50%, 0);
-   opacity:0;
-    transition: all .25s;
-
+  opacity: 0;
+  transition: all 0.25s;
 }
 .slide-down-enter,
 .slide-up-leave-active {
-  transform: translate(0,50%);
-  opacity:1;
-  transition: all .25s;
+  transform: translate(0, 50%);
+  opacity: 1;
+  transition: all 0.25s;
 }
 .slide-down-leave-active,
 .slide-up-enter {
-  transform: translate(0,-50%);
-   opacity:1;
-    transition: all .25s;
-
+  transform: translate(0, -50%);
+  opacity: 1;
+  transition: all 0.25s;
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-  transition-delay: .3s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+  transition-delay: 0.3s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
-.post-content {max-width:75ch;}
+.post-content {
+  max-width: 75ch;
+}
 </style>
